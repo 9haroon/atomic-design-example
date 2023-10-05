75b766b2ad0feba4121c1fc944bd4342f2456416
haroonth <kittisak.ma@logicspark.com>
2023-10-05 11:44:18 +0700

    ➕ update how to import element plus to be automatically

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,37 @@
 import { defineConfig } from "vite";
+import path from "path";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
+import Icons from "unplugin-icons/vite";
+import IconsResolver from "unplugin-icons/resolver";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
+const pathSrc = path.resolve(__dirname, "src");
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
 		AutoImport({
-			resolvers: [ElementPlusResolver()],
+			resolvers: [
+				IconsResolver({ prefix: "Icon" }),
+				ElementPlusResolver({
+					importStyle: "sass",
+				}),
+			],
 		}),
 		Components({
-			resolvers: [ElementPlusResolver()],
+			resolvers: [IconsResolver(), ElementPlusResolver()],
+		}),
+		Icons({
+			autoInstall: true,
 		}),
 	],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				// additionalData: `@use "${pathSrc}/styles/element-plus/index.scss" as *;`,
+			},
+		},
+	},
 });
